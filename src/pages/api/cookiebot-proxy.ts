3a6f52285e132ded15dfdf8dee9720659f3883ed
fandablazek97 <blazek127@gmail.com>
2023-10-23 14:25:38 +0200

    update cookiebot proxy

--- src/pages/api/cookiebot-proxy.ts
+++ src/pages/api/cookiebot-proxy.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 async function cookiebotProxy(req: NextApiRequest, res: NextApiResponse) {
-  const response = await fetch('https://consent.cookiebot.com/uc.js');
+  const response = await fetch("https://consent.cookiebot.com/uc.js");
   const data = await response.text();
   res.send(data);
 }
