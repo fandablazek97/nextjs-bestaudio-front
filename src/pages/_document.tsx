e16c61fae74d4b061c84c3dde538ac49b48db777
fandablazek97 <blazek127@gmail.com>
2023-10-24 11:12:50 +0200

    CB - test 3

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,22 +7,22 @@ export default function Document() {
   return (
     <Html lang="cs" className="scroll-pt-28 scroll-smooth">
       <Head>
-        {/* <Script
-          strategy="beforeInteractive"
+        <Script
+          strategy="afterInteractive"
           id="Cookiebot"
           src="https://consent.cookiebot.com/uc.js"
           data-cbid={cbid}
           data-blockingmode="auto"
           type="text/javascript"
-        /> */}
-        <Script
+        />
+        {/* <Script
           strategy="beforeInteractive"
           id="Cookiebot"
           src="/api/cookiebot-proxy"
           data-cbid={cbid}
           data-blockingmode="auto"  
           type="text/javascript"
-       />
+       /> */}
       </Head>
       <body className="overflow-x-hidden bg-body font-sans text-base font-normal text-copy antialiased transition-colors duration-150 selection:bg-primary/80 selection:text-white">
         <Main />
